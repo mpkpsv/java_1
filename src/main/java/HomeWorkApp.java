68da1a0f490381fd5047f183c9723b55c5c19ab3
mpkpsv <*mpkpsv@gmail.com*>
2021-09-12 18:48:39 +0300

    Практика к 1 уроку

---
+++ src/main/java/HomeWorkApp.java
@@ -0,0 +1,45 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = -10;
+        int s = a + b;
+        if (s >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value = 200;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value <= 100) {
+            System.out.println("Желтый");
+        }
+        if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 10;
+        int b = 5;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+}
